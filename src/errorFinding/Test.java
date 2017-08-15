cd443f22dca9c5f447073bf198846771c3e8791b
acadgild <acadgild@localhost>
2017-08-15 17:59:14 +0530

    Signed-off-by: sridhar

---
+++ src/errorFinding/Test.java
@@ -0,0 +1,29 @@
+/*
+ * 
+ * This program is to demonstrate errors in code (if any) and debugging. 
+ *
+ */
+package errorFinding;
+
+public class Test {
+
+	/*
+	 * Output must be one of the following: a. Compiler error. b. Will throw a
+	 * NoSuchMethod error at runtime. c. It will compile and run printing out
+	 * "10“ d. It will run with no output. e. It will run and print "10" and
+	 * then crash with an error
+	 * 
+	 * 
+	 * Answer is : B , Since main() method is not defined.
+	 */
+	static {
+		print(10);
+	}
+
+	static void print(int x) {
+		System.out.println(x);
+		System.exit(0);
+
+	}
+
+}
