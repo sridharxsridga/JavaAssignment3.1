c6e62e490b6efbae8cd16745a36104aee142432c
acadgild <acadgild@localhost>
2017-08-15 18:10:28 +0530

    Signed-off-by: acadgild <acadgild@localhost>

--- src/errorFinding/Test.java
+++ src/errorFinding/Test.java
@@ -10,11 +10,12 @@ public class Test {
 	/*
 	 * Output must be one of the following: a. Compiler error. b. Will throw a
 	 * NoSuchMethod error at runtime. c. It will compile and run printing out
-	 * "10“ d. It will run with no output. e. It will run and print "10" and
+	 * "10ï¿½ d. It will run with no output. e. It will run and print "10" and
 	 * then crash with an error
 	 * 
 	 * 
-	 * Answer is : B , Since main() method is not defined.
+	 * Answer is : B Will throw a
+	 * NoSuchMethod error at runtime. , Since main() method is not defined.
 	 */
 	static {
 		print(10);
